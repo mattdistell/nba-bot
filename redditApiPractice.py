6a00cd51f5992a69a42452d29d2f68bb4f1bdb17
Matt Distell <mdistell22@gmail.com>
2023-03-22 14:06:35 +0100

    made new file. This is for testing the reddit api functionality

---
+++ redditApiPractice.py
@@ -0,0 +1,13 @@
+import praw
+
+
+r = praw.Reddit('bot1')
+subreddit = r.subreddit("nba")
+
+
+
+for submission in subreddit.hot(limit=5):
+	print("Title:", submission.title)
+	print("Text:", submission.selftext)
+	print("Score:", submission.score)
+	print("---------------------------------\n")
