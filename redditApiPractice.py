597864e2da63f194bfb82269204f18f29e662568
Matt Distell <mdistell22@gmail.com>
2023-03-22 14:40:06 +0100

    more testing on praw api. minor changes

--- redditApiPractice.py
+++ redditApiPractice.py
@@ -1,3 +1,4 @@
+import sunau
 import praw
 
 
@@ -6,8 +7,22 @@ subreddit = r.subreddit("nba")
 
 
 
-for submission in subreddit.hot(limit=5):
-	print("Title:", submission.title)
-	print("Text:", submission.selftext)
-	print("Score:", submission.score)
-	print("---------------------------------\n")
+# for submission in subreddit.hot(limit=5):
+# 	print("Title:", submission.title)
+# 	print("Text:", submission.selftext)
+# 	print("Score:", submission.score)
+# 	print("---------------------------------\n")
+
+
+from praw.models import MoreComments
+
+url = "https://www.reddit.com/r/nbadiscussion/comments/11x1rmo/can_a_below_average_center_still_get_meaningful/"
+
+submission = r.submission(url = url)
+
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    print(comment.body)
+    print("\n")
+
+
