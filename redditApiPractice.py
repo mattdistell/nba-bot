56a1a4be82d4d479ceaec3ed58620da96b483ff3
Matt Distell <mdistell22@gmail.com>
2023-03-23 22:59:52 +0100

    on reddit you can type in identifer plus player's name. Locally able to get ppg, rpg, apg for career from just player's names.

--- redditApiPractice.py
+++ redditApiPractice.py
@@ -2,6 +2,14 @@ import sunau
 import praw
 
 
+from nba_api.stats.endpoints import commonplayerinfo
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.endpoints import PlayerProfileV2
+from nba_api.stats.static import players
+
+
+
+
 r = praw.Reddit('bot1')
 subreddit = r.subreddit("nbadiscussion")
 
@@ -18,20 +26,42 @@ from praw.models import MoreComments
 
 identifer = "!nbastat"
 
-containsIdentifier= False
+
 
 url = "https://www.reddit.com/r/testingzz___zzzz_z/comments/11zx8ij/testtesttest/"
 
 submission = r.submission(url = url)
 
+calls = []
+names = []
+
 submission.comments.replace_more(limit=None)
 for comment in submission.comments.list():
+    containsIdentifier= False
     print(comment.body)
     print("\n")
     if identifer in comment.body:
-        containsIdentifier = True
+        calls.append(comment.body)
+        commentArray = comment.body.split(" ")
+        name = " ".join(commentArray[1:3])
+        print(commentArray)
+        print(name)
+        xTest = players.find_players_by_full_name(name)
+        playerId = xTest[0].get("id")
+        print(playerId)
+        career = PlayerProfileV2(player_id=playerId)
+        # print(career)
+        df = career.get_data_frames()
+        print(df[0])
+        df[0]['ppg'] = df[0]['PTS']/df[0]['GP']
+        df[0]['rpg'] = df[0]['REB']/df[0]['GP']
+        df[0]['apg'] = df[0]['AST']/df[0]['GP']
+        print(df[0]['ppg'])
+        print(df[0]['rpg'])
+        print(df[0]['apg'])
 
-print(containsIdentifier)
+
+
 
 
 
